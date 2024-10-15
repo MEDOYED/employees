bf1af48e6e1a5858214fddc319e7e330b633f475
MEDOYED <mmax26072005@gmail.com>
2024-10-15 10:37:46 +0200

    filter by search

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,13 +1,32 @@
+import { Component } from "react";
+
 import "./search-panel.css";
 
-const SearchPanel = () => {
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="найти сотрудника"
-    />
-  );
-};
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+  }
+
+  onUpdateSearch = e => {
+    const term = e.target.value;
+    this.setState({ term: term });
+    this.props.onUpdateSearch(term); // тута ми передаєм const term (2 строчки вверху) наверх в компонент app.jsx
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
 
 export default SearchPanel;
